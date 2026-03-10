6c5da198d18ab7c4b3ee4ca7683bf092b10bf8f5
m4nik1 <maniksoomro@hotmail.com>
2026-03-10 09:49:50 -0400

    Added button and visualizer for audio input

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,14 +1,41 @@
-import { useState } from "react";
-import { Orb } from "@/components/ui/orb";
+"use client";
+import { AgentState, BarVisualizer } from "@/components/ui/bar-visualizer";
+import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [agentState, setAgentState] = useState<
-    "thinking" | "listening" | "talking" | null
-  >(null);
+  const [agentState, setAgentState] = useState<AgentState>("initializing");
+
+  function activateAI() {
+    console.log("pressed button!");
+    setAgentState("listening");
+
+    setTimeout(() => {
+      setAgentState("thinking");
+    }, 3000);
+
+    setAgentState("listening");
+    setAgentState("speaking");
+    setAgentState("listening");
+  }
+
+  useEffect(() => {
+    console.log(agentState);
+  }, [agentState]);
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Orb volumeMode="manual" />
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 px-6 font-sans dark:bg-black">
+      <div className="flex w-full max-w-xl flex-col items-center gap-6">
+        <BarVisualizer
+          state={agentState}
+          barCount={15}
+          centerAlign={true}
+          className="w-full"
+        />
+        <Button className="cursor-pointer" onClick={() => activateAI()}>
+          Start
+        </Button>
+      </div>
     </div>
   );
 }
